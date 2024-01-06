463cc69e492baef1a52baa637b8d810b29a04025
RoshanKhalinG <102815285+RoshanKhalinG@users.noreply.github.com>
2024-01-06 21:24:43 +0545

    wpm

---
+++ app.js
@@ -0,0 +1,13 @@
+function setTheme(themeName) {
+    localStorage.setItem("theme", themeName);
+    document.documentElement.className = themeName;
+  }
+  
+  // function to toggle between light and dark theme
+  function toggleTheme() {
+    if (localStorage.getItem("theme") === "theme-dark") {
+      setTheme("theme-light");
+    } else {
+      setTheme("theme-dark");
+    }
+  }
