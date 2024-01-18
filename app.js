d56235b300de13e3610d840bec6189f7f79dafdd
RoshanKhalinG <102815285+RoshanKhalinG@users.noreply.github.com>
2024-01-18 22:28:33 +0545

    wpm

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+const local = document.getElementById("local");
+const timer_text = document.querySelector(".curr_time");
+const first_row = document.getElementById("firstrow");
+const level = document.getElementById("select");
+
+const timeModes = {
+  15: document.getElementById("fifteen"),
+  30: document.getElementById("thirty"),
+  60: document.getElementById("sixty"),
+  120: document.getElementById("twomin"),
+};
+
+
+
+
+
 function setTheme(themeName) {
     localStorage.setItem("theme", themeName);
     document.documentElement.className = themeName;
