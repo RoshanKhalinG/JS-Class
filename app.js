840f7255c4deada57c77dadc9e3727714952fcbf
RoshanKhalinG <102815285+RoshanKhalinG@users.noreply.github.com>
2024-01-22 22:05:24 +0545

    wpm

--- app.js
+++ app.js
@@ -9,22 +9,3 @@ const timeModes = {
   60: document.getElementById("sixty"),
   120: document.getElementById("twomin"),
 };
-
-
-
-
-
-function setTheme(themeName) {
-    localStorage.setItem("theme", themeName);
-    document.documentElement.className = themeName;
-  }
-  
-  // function to toggle between light and dark theme
-  function toggleTheme() {
-    if (localStorage.getItem("theme") === "theme-dark") {
-      setTheme("theme-light");
-      setheadercolor("Pink");
-    } else {
-      setTheme("theme-dark");
-    }
-  }
---
+++ theme.js
@@ -0,0 +1,16 @@
+
+
+function setTheme(themeName) {
+    localStorage.setItem("theme", themeName);
+    document.documentElement.className = themeName;
+  }
+  
+  // function to toggle between light and dark theme
+  function toggleTheme() {
+    if (localStorage.getItem("theme") === "theme-dark") {
+      setTheme("theme-light");
+      setheadercolor("Pink");
+    } else {
+      setTheme("theme-dark");
+    }
+  }
